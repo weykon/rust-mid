022858e506c88ac1a4cbe79d5490da085ed16d5d
weykon <635883092@qq.com>
2024-01-22 17:53:03 +0800

    size of a &T

--- fat_or_not/src/main.rs
+++ fat_or_not/src/main.rs
@@ -8,6 +8,8 @@ fn main() {
 
     // 这里讨论关于类型和指针到类型的实际内存下的视野的事情
     // 以下讨论都基于64位系统下，所以在指针的长度上都是8字节（64位/8位一字节 = 8）
-    // &Cat 这个用一个8位空间保存
-    // &<dyn Animal>而Animal作为了trait是用fat指针来保存，有16位空间
+    // &Cat 这个用8个字节保存
+    // &<dyn Animal>而Animal作为了trait是用fat指针来保存，有16个字节
+
+    println!("size of &Cat: {}", std::mem::size_of::<&Cat>());
 }
